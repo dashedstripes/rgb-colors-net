511c9ea71ccea376e570818d2422008feeed0232
Adam Gray <adamgrayscale@gmail.com>
2022-12-19 15:28:35 -0600

    configureable num of rows

--- rgb_data_gen.py
+++ rgb_data_gen.py
@@ -48,16 +48,16 @@ def generate_color(color="red"):
   r, g, b = hls_to_rgb(h/360, l/100, s/100)
   return [round(r*255.0), round(g*255.0), round(b*255.0)]
 
-def generate_csv(filename='rgb_train.csv'):
+def generate_csv(num_of_rows=50000, filename='rgb_train.csv'):
   with open(filename, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
     writer.writerow(['red', 'green', 'blue', 'label'])
 
-    for _ in range (50000):
+    for _ in range (num_of_rows):
       label = random.choice(hue_choices)
       color = generate_color(label)
       writer.writerow([color[0], color[1], color[2], label])
 
-generate_csv('rgb_train.csv')
-generate_csv('rgb_test.csv')
+generate_csv(num_of_rows=50000, filename='rgb_train.csv')
+generate_csv(num_of_rows=50000, filename='rgb_test.csv')
